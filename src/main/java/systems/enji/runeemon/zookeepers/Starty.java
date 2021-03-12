089db2a1245c397b19c65bb1fed7e60a61954251
enji7 <enji@bounty>
2021-03-12 16:35:47 +0100

    command execution improvements

--- src/main/java/systems/enji/runeemon/zookeepers/Starty.java
+++ src/main/java/systems/enji/runeemon/zookeepers/Starty.java
@@ -1,8 +1,14 @@
 package systems.enji.runeemon.zookeepers;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import systems.enji.runeemon.data.CommandData;
 import systems.enji.runeemon.data.RuntimeData;
@@ -32,7 +38,7 @@ public class Starty {
     if (command == null || command.isBlank()) {
       command = runtime.getStartForegroundCommand();
     }
-    exec(runtime.getExtractDir().getAbsolutePath() + command);
+    exec(runtime.getExtractDir(), command);
   }
   
   private static void startBackground(RuntimeData runtime) {
@@ -41,7 +47,7 @@ public class Starty {
       System.out.printf("can't start %s in the background because it has no explicit background start command\n", runtime.getName());
       return;
     }
-    exec(runtime.getExtractDir().getAbsolutePath() + command);
+    exec(runtime.getExtractDir(), command);
   }
   
   private static void startForeground(RuntimeData runtime) {
@@ -50,7 +56,7 @@ public class Starty {
       System.out.printf("can't start %s in the foreground because it has no explicit foreground start command\n", runtime.getName());
       return;
     }
-    exec(runtime.getExtractDir().getAbsolutePath() + command);
+    exec(runtime.getExtractDir(), command);
   }
   
   private static void stop(RuntimeData runtime) {
@@ -59,19 +65,72 @@ public class Starty {
       System.out.printf("can't stop %s because it has no explicit stop command\n", runtime.getName());
       return;
     }
-    exec(runtime.getExtractDir().getAbsolutePath() + command);
+    exec(runtime.getExtractDir(), command);
   }
   
-  private static void exec(String command) {
+  private static void exec(File runtimeDir, String command) {
+    
+    ProcessBuilder pb = new ProcessBuilder(assembleCommand(runtimeDir, command));
+    
+    // direct entry for TomEE; might move to configuration one day
+    pb.environment().put("CATALINA_HOME", runtimeDir.getAbsolutePath());
+    
+    if (Ossy.isWindows()) {
+      pb.redirectOutput(Redirect.INHERIT);
+      pb.redirectError(Redirect.INHERIT);
+    }
+
     try {
-      Process process = Runtime.getRuntime().exec(command);
-      new BufferedReader(new InputStreamReader(process.getInputStream())).lines().forEach(System.out::println);
-      int exitCode = process.waitFor();
-      System.out.println("exit code: " + exitCode);
       
-    } catch (IOException | InterruptedException e) {
+      Process process = pb.start();
+      
+      if (!Ossy.isWindows()) {
+        
+        // on Linux, we just have to take care of reading everything that comes out of the started process...
+        consumeOutput(process);
+        
+        // ...and wait until the process has finished
+        process.waitFor();
+        
+      }
+      else {
+        // on Windows, the Linux solution above might hang forever;
+        // instead, we have redirected all output to that of the current process above
+        // and now wait a little bit to give the server the opportunity to spawn;
+        // drawback of this solution: the original command prompt will pop up after this sleep time, no matter
+        // if the invoked operation has finished or not
+        Thread.sleep(1000);
+      }
+
+    }
+    catch (IOException | InterruptedException e) {
       throw new RuntimeException(e);
     }
+
   }
-  
+
+  private static List<String> assembleCommand(File runtimeDir, String command) {
+    
+    List<String> commandList = new LinkedList<>();
+    if (Ossy.isWindows()) {
+      commandList.add("cmd");
+      commandList.add("/c");
+    }
+    commandList.addAll(Arrays.asList((runtimeDir.getAbsolutePath() + command).split(" ")));
+    
+    System.out.println("executing: " + commandList.stream().collect(Collectors.joining(" ")));
+    
+    return commandList;
+    
+  }
+
+  private static void consumeOutput(Process process) {
+    new Thread(() -> {
+      new BufferedReader(new InputStreamReader(process.getInputStream())).lines().forEach(System.out::println);
+    }).start();
+    new Thread(() -> {
+      new BufferedReader(new InputStreamReader(process.getErrorStream())).lines().forEach(System.out::println);
+    }).start();
+  }
+
 }
